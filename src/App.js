9f44896b472d8a40af69bcf31f7c1356fb4c8cc9
Aquila <akilatouati97@gmail.com>
2021-08-05 11:58:35 +0100

    add form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="input">
+      <span class="input-group-text">First and last name</span>
+      <input
+        type="text"
+        aria-label="First name"
+        class="form-control"
+        placeholder="First Name"
+      />
+      <input
+        type="text"
+        aria-label="Last name"
+        class="form-control"
+        placeholder="Last Name"
+      />
+
+      <div class="secondinput">
+        <span class="text" id="addon-wrapping"></span>
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Email @"
+          aria-label="Username"
+          aria-describedby="addon-wrapping"
+        />
+      </div>
+      <label for="inputPassword" class="col-sm-2 col-form-label">
+        Password
+      </label>
+      <div class="col-sm-10">
+        <input type="password" class="form-control" id="inputPassword" />
+      </div>
     </div>
   );
 }
