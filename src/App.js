cbc9e81bd5a18cdd51cba3a539dcff82c0ef662c
Aquila <akilatouati97@gmail.com>
2021-08-09 17:01:37 +0100

    jsx checkpoint

--- src/App.js
+++ src/App.js
@@ -1,40 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import imagePublic from "/imageInPublic.jpg";
 
 function App() {
   return (
-    <div className="input">
-      <span class="input-group-text">First and last name</span>
-      <input
-        type="text"
-        aria-label="First name"
-        class="form-control"
-        placeholder="First Name"
-      />
-      <input
-        type="text"
-        aria-label="Last name"
-        class="form-control"
-        placeholder="Last Name"
-      />
+    <div>
+      <div style={{ border: "solid 1 black", maxWidth: "100vw" }} />
 
-      <div class="secondinput">
-        <span class="text" id="addon-wrapping"></span>
-        <input
-          type="text"
-          class="form-control"
-          placeholder="Email @"
-          aria-label="Username"
-          aria-describedby="addon-wrapping"
-        />
-      </div>
-      <label for="inputPassword" class="col-sm-2 col-form-label">
-        Password
-      </label>
-      <div class="col-sm-10">
-        <input type="password" class="form-control" id="inputPassword" />
-      </div>
+      <h1 className="title red">Your name here</h1>
+
+      <br />
+
+      <img src={"/imageInSrc.jpg"} />
+
+      <br />
+
+      <img src={imagePublic} />
+
+      <video style={{ width: "320", height: "240", controls }}>
+        <source src={"myVideo.mp4"} type="video/mp4" />
+      </video>
     </div>
   );
 }
